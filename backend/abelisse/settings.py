41b359d3e281c91f8399b23bead7328534c3f84a
ABELISSE <lfalcon2311us@gmail.com>
2026-02-17 09:28:03 -0500

    Configure CORS settings in settings.py
    Add CORS settings to allow specific headers and methods.

--- backend/abelisse/settings.py
+++ backend/abelisse/settings.py
@@ -123,3 +123,23 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = []
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
