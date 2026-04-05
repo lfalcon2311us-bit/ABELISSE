2eaddab80f0f5431f55bb41fab591d5f50e2f6cf
ABELISSEbeauty <lfalcon2311us@gmail.com>
2026-04-05 09:57:03 -0400

    update

--- backend/pagos/views_geo.py
+++ backend/pagos/views_geo.py
@@ -5,14 +5,13 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def detectar_pais(request):
     try:
-        # Llamada desde el backend → NO tiene CORS
-        res = requests.get("https://ipapi.co/json/")
+        res = requests.get("https://ipwho.is/")
         data = res.json()
 
-        pais = data.get("country_code", "US")  # fallback seguro
+        pais = data.get("country_code", "US")
 
         return JsonResponse({"country": pais})
 
     except Exception as e:
         print("❌ Error detectando país:", e)
-        return JsonResponse({"country": "US"})  # fallback
+        return JsonResponse({"country": "US"})
