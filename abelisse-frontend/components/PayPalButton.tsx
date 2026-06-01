b7209436e112227674414acdefdf3c2ae4fd1567
ABELISSE <lfalcon2311us@gmail.com>
2026-06-01 19:21:54 -0400

    Update PayPal SDK script source for funding options

--- abelisse-frontend/components/PayPalButton.tsx
+++ abelisse-frontend/components/PayPalButton.tsx
@@ -33,7 +33,7 @@ export default function PayPalButton({
     script.id = "paypal-sdk";
 
     // ⭐ SDK COMPLETO: PayPal + Pay Later + Tarjeta
-    script.src = `https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}&currency=${currency}&intent=CAPTURE&components=buttons,hosted-fields&enable-funding=card,paylater`;
+    script.src = `https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}&currency=${currency}&intent=CAPTURE&components=buttons&enable-funding=paylater`;
     script.async = true;
 
     script.onload = () => {
