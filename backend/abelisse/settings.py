724adb83dd28a54f4f3987a3d896915e316ed15f
ABELISSE <lfalcon2311us@gmail.com>
2026-06-01 01:20:13 -0400

    Change STATIC_URL to '/staticfiles/'

--- backend/abelisse/settings.py
+++ backend/abelisse/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # ============================
 #   STATIC FILES
 # ============================
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
