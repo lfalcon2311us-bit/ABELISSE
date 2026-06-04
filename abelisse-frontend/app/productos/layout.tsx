16f10a398d43034b51e2248ff94d8689951c69a1
ABELISSE <lfalcon2311us@gmail.com>
2026-06-04 08:41:21 -0400

    Update layout.tsx

--- abelisse-frontend/app/productos/layout.tsx
+++ abelisse-frontend/app/productos/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
-export default function ProductosLayout({ children }) {
+import { ReactNode } from "react";
+
+export default function ProductosLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
