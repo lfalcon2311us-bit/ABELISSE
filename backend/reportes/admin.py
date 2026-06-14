3026624fe5b94ad8ee15f1e00d68e32336582fcd
ABELISSEbeauty <lfalcon2311us@gmail.com>
2026-06-13 22:20:10 -0400

    update

--- backend/reportes/admin.py
+++ backend/reportes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ErrorReport
 
-# Register your models here.
+@admin.register(ErrorReport)
+class ErrorReportAdmin(admin.ModelAdmin):
+    list_display = ("id", "message", "timestamp")
+    list_filter = ("timestamp",)
+    search_fields = ("message", "error", "context")
+    ordering = ("-timestamp",)
