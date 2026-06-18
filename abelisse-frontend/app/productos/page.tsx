1f4aa9f311161c50ff1875eba58dda203103dc59
ABELISSEbeauty <lfalcon2311us@gmail.com>
2026-06-18 19:04:19 -0400

    update

--- abelisse-frontend/app/productos/page.tsx
+++ abelisse-frontend/app/productos/page.tsx
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 import ProductCardPremium from "@/components/ProductCardPremium";
 import { getProductos } from "@/lib/api";
 
